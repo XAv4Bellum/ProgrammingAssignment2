30e2f579954258edaef199b47f1793ac51b15e27
XAv4Bellum <belles.xavier@skynet.be>
2015-07-26 23:11:04 +0200

    successfull debugging for a two by two matrix and completing all comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,37 +4,50 @@
 ##      "matrix" returned by `makeCacheMatrix` above. If the inverse has
 ##      already been calculated (and the matrix has not changed), then
 ##`     cacheSolve` should retrieve the inverse from the cache.
-##      functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(mymatrix = matrix()) {
+
+# The function "makeCacheMatrix" takes a square inversible matrix as argument and returns
+# a list with 4 functions. This function is an adaptation of the makeVector function.
+makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        
+# function "set" is setting value of matrix in another environment than current one.        
         set <- function(y) {
-                mymatrix <<- y
+                x <<- y
                 m <<- NULL
         }
-        get <- function() mymatrix
+# function "get" is getting value of stored matrix.
+        get <- function() x
+# function "setsolve" is setting value of inverted matrix in another environment than current one.
         setsolve <- function(solve) m <<- solve
+# function "getsolve" is getting value of stored inverted matrix.        
         getsolve <- function() m
+#list with 4 functions returned by makeCacheMatrix.
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(mymatrix, ...) {
-        ## Return a matrix that is the inverse of 'mymatrix'
-        m <- mymatrix$getsolve()
+# The function "cacheSolve" takes the return of the makeCacheMatrix function as argument and 
+# assumes that a square inversible matrix was provided to the makeCacheMatrix function.
+# This function checks if an inverted square matrix was cached, if not the solve function
+# is used to invert the square inversible matrix from the makeCacheMatrix function.
+# This function is an adaptation of the cachemean function.
+
+cacheSolve <- function(x) {
+# checking if an inverted matrix was already stored in cache in x*$getsolve
+        m <- x$getsolve()
+# if yes, then warning message and function is returning inverted matrix from cache
         if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+        message("getting cached data")
+        return(m)
         }
-        data <- mymatrix$get()
-        m <- solve(data, ...)
-        mymatrix$setsolve(m)
+#if not, getting the stored square inversible matrix and using the solve function
+#to compute its inverted matrix then storing and returning it.
+        data <- x$get()
+        m <- solve(data)
+        x$setsolve(m)
         m
         }
